a6c49a90071cc151d964e684ac1e825303a2b008
anemadhon <anemadhon@gmail.com>
2023-01-12 11:32:21 +0700

    add id

--- src/Domains/threads/entities/Thread.js
+++ src/Domains/threads/entities/Thread.js
@@ -1,11 +1,19 @@
 class Thread {
-	constructor({ title, body }) {
-		if (!title || !body) {
-			throw new Error('CREATE_THREAD.INVALID_PAYLOAD')
-		}
+	constructor({ id, title, body }) {
+		this._verifyPayload({ id, title, body })
 
+		this.id = id
 		this.title = title
 		this.body = body
+	}
+
+	_verifyPayload({ id, title, body }) {
+		if (!id || !title || !body) {
+			throw new Error('CREATE_THREAD.INVALID_PAYLOAD')
+		}
+		if (typeof id !== 'string') {
+			throw new Error('CREATE_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION')
+		}
 	}
 }
 
--- src/Domains/threads/entities/_test/Thread.test.js
+++ src/Domains/threads/entities/_test/Thread.test.js
@@ -8,10 +8,22 @@ describe('a Thread entities', () => {
 			'CREATE_THREAD.INVALID_PAYLOAD'
 		)
 	})
+	it('should throw an error when id is integer', () => {
+		const payload = { id: 123, title: 'title', body: 'body' }
+
+		expect(() => new Thread(payload)).toThrowError(
+			'CREATE_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION'
+		)
+	})
 	it('should created Thread object correctly', () => {
-		const payload = { title: 'title', body: 'body' }
+		const payload = {
+			id: 'thread-123qweasdzxc890',
+			title: 'title',
+			body: 'body',
+		}
 		const thread = new Thread(payload)
 
+		expect(thread.id).toEqual(payload.id)
 		expect(thread.title).toEqual(payload.title)
 		expect(thread.body).toEqual(payload.body)
 	})
