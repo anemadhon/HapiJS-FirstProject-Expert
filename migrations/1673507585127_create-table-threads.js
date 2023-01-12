4b42cd81e45f66b10c7cefeeb6f7a9149c5cb92e
anemadhon <anemadhon@gmail.com>
2023-01-12 14:36:09 +0700

    add thread migrations

---
+++ migrations/1673507585127_create-table-threads.js
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+exports.up = pgm => {
+	pgm.createTable('threads', {
+		id: {
+			type: 'VARCHAR(50)',
+			primaryKey: true,
+		},
+		title: {
+			type: 'VARCHAR(50)',
+			notNull: true,
+		},
+		body: {
+			type: 'VARCHAR(50)',
+			notNull: true,
+		},
+	})
+}
+
+exports.down = pgm => pgm.dropTable('threads')
