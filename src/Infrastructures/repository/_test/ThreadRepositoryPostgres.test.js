8ce244be455a88fc1bdd5fd4a2c48b2eb7a9b15c
anemadhon <anemadhon@gmail.com>
2023-01-13 11:24:44 +0700

    modify test case

--- src/Infrastructures/repository/_test/ThreadRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/ThreadRepositoryPostgres.test.js
@@ -1,10 +1,8 @@
 const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper')
 const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper')
-const RegisterUser = require('../../../Domains/users/entities/RegisterUser')
 const AddThread = require('../../../Domains/threads/entities/AddThread')
 const Thread = require('../../../Domains/threads/entities/Thread')
 const pool = require('../../database/postgres/pool')
-const UserRepositoryPostgres = require('../UserRepositoryPostgres')
 const ThreadRepositoryPostgres = require('../ThreadRepositoryPostgres')
 
 describe('a ThreadRepositoryPostgres', () => {
@@ -19,26 +17,13 @@ describe('a ThreadRepositoryPostgres', () => {
 
 	describe('addThread function', () => {
 		it('should persist add a thread', async () => {
-			// Arrange
-			const registerUser = new RegisterUser({
-				username: 'dicoding',
-				password: 'secret_password',
-				fullname: 'Dicoding Indonesia',
-			})
-			const fakeIdGenerator = () => '123' // stub!
-			const userRepositoryPostgres = new UserRepositoryPostgres(
-				pool,
-				fakeIdGenerator
-			)
+			await UsersTableTestHelper.addUser({ id: 'user-123' })
 
-			// Action
-			await userRepositoryPostgres.addUser(registerUser)
-
-			// Assert
 			const users = await UsersTableTestHelper.findUsersById('user-123')
 
 			expect(users).toHaveLength(1)
 
+			const fakeIdGenerator = () => '123' // stub!
 			const thread = new AddThread({
 				title: 'title',
 				body: 'body',
@@ -59,27 +44,13 @@ describe('a ThreadRepositoryPostgres', () => {
 		})
 
 		it('should return added thread correctly', async () => {
-			// Arrange
-			const registerUser = new RegisterUser({
-				username: 'dicoding',
-				password: 'secret_password',
-				fullname: 'Dicoding Indonesia',
-			})
+			await UsersTableTestHelper.addUser({ id: 'user-123' })
 
-			const fakeIdGenerator = () => '123' // stub!
-			const userRepositoryPostgres = new UserRepositoryPostgres(
-				pool,
-				fakeIdGenerator
-			)
-
-			// Action
-			await userRepositoryPostgres.addUser(registerUser)
-
-			// Assert
 			const users = await UsersTableTestHelper.findUsersById('user-123')
 
 			expect(users).toHaveLength(1)
 
+			const fakeIdGenerator = () => '123' // stub!
 			const thread = new AddThread({
 				title: 'title',
 				body: 'body',
