09ba12fadbc6303c12329344a8e98f1e80868eea
anemadhon <anemadhon@gmail.com>
2023-01-13 09:47:54 +0700

    add new method in thread repo

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -2,6 +2,9 @@ class ThreadRepository {
 	async addThread() {
 		throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED')
 	}
+	async getThreadById() {
+		throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+	}
 }
 
 module.exports = ThreadRepository
--- src/Domains/threads/_test/ThreadRepository.test.js
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -8,4 +8,11 @@ describe('ThreadRepository interface', () => {
 			'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
 		)
 	})
+	it('should throw an error when invoke abtract behavior', async () => {
+		const threadRepo = new ThreadRepository()
+
+		await expect(threadRepo.getThreadById({})).rejects.toThrowError(
+			'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED'
+		)
+	})
 })
