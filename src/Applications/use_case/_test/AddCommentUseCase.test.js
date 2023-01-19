8f56df9ca1bfd8f6fb673893d2251e2c8f0200f5
anemadhon <anemadhon@gmail.com>
2023-01-19 23:27:25 +0700

    add thread repo for checking thread

--- src/Applications/use_case/_test/AddCommentUseCase.test.js
+++ src/Applications/use_case/_test/AddCommentUseCase.test.js
@@ -1,6 +1,7 @@
 const AddComment = require('../../../Domains/comments/entities/AddComment')
 const Comment = require('../../../Domains/comments/entities/Comment')
 const CommentRepository = require('../../../Domains/comments/CommentRepository')
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository')
 const AddCommentUseCase = require('../AddCommentUseCase')
 
 describe('a AddCommentUseCase', () => {
@@ -15,14 +16,19 @@ describe('a AddCommentUseCase', () => {
 			content: commentUseCasePayload.content,
 			owner: 'user-123',
 		})
+		const threadRepoMocked = new ThreadRepository()
 		const commentRepoMocked = new CommentRepository()
 
+		threadRepoMocked.getThreadById = jest
+			.fn()
+			.mockImplementation(() => Promise.resolve())
 		commentRepoMocked.addComment = jest
 			.fn()
 			.mockImplementation(() => Promise.resolve(expectedAddedComment))
 
 		const getAddCommentUseCase = new AddCommentUseCase({
 			commentRepository: commentRepoMocked,
+			threadRepository: threadRepoMocked,
 		})
 		const addedComment = await getAddCommentUseCase.execute(
 			commentUseCasePayload
